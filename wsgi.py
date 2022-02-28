9120a8bb5f952b0946f3de0ce9831cc6d5c9b582
Lei Wang <leiw@juniper.net>
2022-02-28 14:39:56 +0800

    read to deploy

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-from watchlist import app
+#from watchlist import app
